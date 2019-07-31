861ae35c795406e6ca1d84afc5d31a3eb1e3625d
Tyler Koepke <tylerkoepke@Tylers-MacBook-Air.local>
2019-07-31 13:31:47 -0500

    init

---
+++ rpsls/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rpsls
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
